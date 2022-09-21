4b295ef4bc5567e650ced1028266f518aa859612
Sebastian Vega <svegalopez@gmail.com>
2022-09-21 13:55:47 -0400

    updates

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,12 @@
 describe('Basic', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3000/hermit-client');
     cy.get('.app').should('exist');
+  })
+
+  it('it should render a welcome heading', () => {
+    cy.visit('http://localhost:3000/hermit-client');
+    cy.get('.app').find('h1').contains('Welcome');
   })
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import "./App.scss";
 import React from "react";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>Welcome!</h1>
+    </div>
+  );
 }
 
 export default App;
