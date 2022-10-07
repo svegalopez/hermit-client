43e01045d780a286fa80c77329727747d2dc57d6
Sebastian Vega <svegalopez@gmail.com>
2022-10-07 15:07:58 -0400

    Update getHost.ts

--- src/utils/getHost.ts
+++ src/utils/getHost.ts
@@ -1,13 +1,16 @@
 export default function getHost() {
     if (process.env.REACT_APP_HERMIT_HOST) {
+        console.log(3);
         return process.env.REACT_APP_HERMIT_HOST;
     }
 
     if (window.location.hostname === 'localhost') {
+        console.log(8);
         return 'https://hermit-server-staging.herokuapp.com';
     }
 
     if (window.location.hostname === '127.0.0.1') {
+        console.log(13);
         return 'https://hermit-server-staging.herokuapp.com';
     }
 
