8b5db2d74567eef4b0b54386765221309430d075
Sebastian Vega <svegalopez@gmail.com>
2022-10-13 17:05:02 -0400

    updates to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,14 @@
 import { useContext } from "react";
 import AuthCtx from "../../context/authContext";
+import hermit from "./hermit.png";
+import "./Home.scss";
 
 export default function Home() {
   const auth = useContext(AuthCtx);
-  return <></>;
+  return (
+    <div className="home">
+      <h1>Hermit</h1>
+      <img src={hermit} alt="a red hermit crab" className="hermit-img" />
+    </div>
+  );
 }
